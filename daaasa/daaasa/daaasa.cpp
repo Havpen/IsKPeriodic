ef1d492aa0f1808dbb96bf922199493907011b34
Havpen <zhenya_alisievich@mail.ru>
2024-04-04 19:00:25 +0300

    Added dasaaa.cpp file with main code

---
+++ daaasa/daaasa/daaasa.cpp
@@ -0,0 +1,43 @@
+﻿#include <iostream>
+#include <string>
+
+
+bool IsKPeriodic(std::string str, int K) {
+    int length = str.length();
+    if (length % K != 0) {
+        return false;
+    }
+    std::string pat = str.substr(0, K);
+    for (int i = K; i < length; i += K) {
+        if (str.substr(i, K) != pat) {
+            return false;
+        }
+        return true;
+    }
+}
+
+int main() {
+
+    std::string k = "acacacacacacac";
+    if (IsKPeriodic(k, 2)) {
+        std::cout << "true";
+    }
+    else { std::cout << "false"; }
+
+    std::cout<<std::endl;
+
+    std::string h = "abcdabcdabcd";
+    if (IsKPeriodic(h, 4)) {
+        std::cout << "true";
+    }
+    else { std::cout << "false"; }
+
+    std::cout << std::endl;
+
+    if (IsKPeriodic(h, 3)) {
+        std::cout << "true";
+    }
+    else { std::cout << "false"; }
+
+    return 0;
+}
